5090761ee6a62e32f1aa3ea49cd10e7c5d9a1684
Ash-mitanwar <ashmitharyana@gmail.com>
2025-05-23 12:48:52 +0530

    Portfolio

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Scroll effect placeholder (highlight nav, etc.)
 window.addEventListener("scroll", () => {
     const nav = document.querySelector(".navbar");
     nav.style.backgroundColor = window.scrollY > 50 ? "#1f1f1f" : "transparent";
